478892aff1423530b330d8f4c3ed4bf2b41c5e9e
Katie Fujihara and Kimberly Huynh <kimberly@>
2018-01-18 10:57:46 -0800

    add way to the words beginning with a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,25 @@
+var vowels= ["a","e","i","o","u"];
+
+
+
 function piglatin(){
 
 var phrase=$("input#phrase").val();
 var splitPhrase = phrase.split("");
-if(splitPhrase[0]==="y"){
-  
-} else {
-  alert("doesn't start with y")
+var firstLetter= splitPhrase[0]
+
+ for(var i=0;i<vowels.length;i++){
+  if (vowels[i] === firstLetter) {
+    $(".result").text(phrase+"way")
+  }
 }
 };
 
-$(document).ready(function() {
 
+$(document).ready(function() {
   $("#translator").submit(function(event) {
     event.preventDefault();
     piglatin();
-
+    $(".result").show();
   });
 });
