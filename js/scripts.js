0b228d76e61a6dfa17c56df034eeac3b32e0c584
Katie Fujihara and Kimberly Huynh <katie@>
2018-01-18 16:08:23 -0800

    this doesnt work

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,27 @@
-var vowels= ["a","e","i","o","u"];
+  var vowels= ["a","e","i","o","u"];
 
+function piglatin(phrase){
+  var firstLetter= phrase[0];
+  var firstAndSecond= phrase[0,1];
+  var splitPhrase= phrase.split("");
+  var consonant= splitPhrase.splice(0,2)
+  var finalConsonant=phrase.substring(phrase.length );
+  var finalPhrase=" ";
+  console.log(finalPhrase);
 
-
-function piglatin(){
-
-var phrase=$("input#phrase").val();
-var splitPhrase = phrase.split("");
-var firstLetter= splitPhrase[0]
-
- for(var i=0;i<vowels.length;i++){
-  if (vowels[i] === firstLetter) {
-    $(".result").text(phrase+"way")
-  }
-}
+    if (vowels.includes(firstLetter)) {
+      finalPhrase=phrase+"way"
+    } else {
+      finalPhrase=phrase+"ay"
+    };
 };
-
 
 $(document).ready(function() {
   $("#translator").submit(function(event) {
     event.preventDefault();
-    piglatin();
+    var phrase=$("input#phrase").val();
+    piglatin(phrase);
     $(".result").show();
+    $(finalPhrase).append(".result");
   });
 });
